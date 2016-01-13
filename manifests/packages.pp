fd80c76308cd4537e79cf9666056f2ccf05f50eb
Felipe F Quintella <felipe.quintella@fgv.br>
2016-01-12 22:15:18 -0200

    Change to make it generic

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -1,41 +1,49 @@
 # This controls the package dependdencies that Golang needs.
 # This is controlled by the
-# $manage_dependencies param being set to true or false 
+# $manage_dependencies param being set to true or false
 
 class golang::packages {
-  
-  package { 'gcc':
-  ensure => installed,
-  }->
+
+  if(!defined(Package['gcc'])){
+    package { 'gcc':
+      ensure => installed,
+    }
+  }
+
 
   case $::osfamily {
-  
+
   'RedHat': {
-  
-  package { 'glibc-devel':
-  ensure => installed,
+
+    package { 'glibc-devel':
+      ensure => installed,
+      require => Package['gcc']
     }
   }
-  
+
   'Debian': {
-  
-  package {'libc6-dev':
-  ensure => installed,
-  } ->
 
-  package {'bison':
-  ensure => installed,
-  } ->
+    package {'libc6-dev':
+      ensure => installed,
+      require => Package['gcc']
+    }
 
-  package {'make':
-  ensure => installed,
-  } ->
+    package {'bison':
+      ensure => installed,
+      require => [Package['gcc'],Package['libc6-dev']]
+    }
 
-  package {'build-essential':
-  ensure => installed,
+    package {'make':
+      ensure => installed,
+      require => [Package['gcc'], Package['libc6-dev'], Package['bison']]
+    }
+
+    package {'build-essential':
+      ensure => installed,
+      require => [Package['gcc'], Package['libc6-dev'], Package['bison'], Package['make']]
     }
   }
-  
+
   default: { notify {"${::osfamily} is not supported by this module":}
     }
   }
